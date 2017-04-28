758cd4d0e0c367b387e15147e19129eb428cdb83
Ryan Schmukler <rschmukler@gmail.com>
2017-04-28 13:01:58 -0400

    feat(extra): port over Keyword.Extra module and tests

---
+++ lib/keyword/extra.ex
@@ -0,0 +1,88 @@
+defmodule Keyword.Extra do
+  @moduledoc """
+  Extensions to the built-in `Keyword` module.
+
+  """
+
+  @type key :: Keyword.key
+  @type value :: Keyword.value
+
+  @type t :: Keyword.t
+
+
+  @doc """
+  Takes all entries corresponding to the given keys and returns them in a new
+  keyword list in the order that `keys` arrived.
+
+  ## Examples
+
+    iex> Keyword.Extra.take_ordered([c: 3, b: 2, a: 1], [:a, :b])
+    [a: 1, b: 2]
+
+    iex> Keyword.Extra.take_ordered([x: 1, y: 2], [:x, :y, :z])
+    [x: 1, y: 2]
+
+    iex> location = %{state: "NY", city: "Manhattan", zip_code: "10009"}
+    ...> Keyword.Extra.take_ordered(location, [:city, :state, :zip_code])
+    [city: "Manhattan", state: "NY", zip_code: "10009"]
+
+  """
+  @spec take_ordered(t, [key]) :: t
+  @spec take_ordered(map, [key]) :: t
+  def take_ordered([{key, _value} | _rest] = kw_list, keys)
+    when is_atom(key),
+    do: do_take_ordered(kw_list, keys)
+
+  def take_ordered(%{} = map, keys) do
+    map
+    |> Keyword.new
+    |> do_take_ordered(keys)
+  end
+
+  @spec do_take_ordered(t, [key]) :: t
+  defp do_take_ordered(kw_list, keys) do
+    keys
+    |> Stream.filter(&Keyword.has_key?(kw_list, &1))
+    |> Enum.map(&get_pair(kw_list, &1))
+  end
+
+  @spec get_pair(t, key) :: {key, value} | nil
+  defp get_pair(kw_list, key) do
+    with {:ok, value} <- Keyword.fetch(kw_list, key) do
+      {key, value}
+    else
+      :error -> nil
+    end
+  end
+
+
+  @doc """
+  Raises an ArgumentError error if `list` contains the provided `key`.
+
+  Takes an optional argument specifying the error message.
+
+  """
+  @spec refute_key!(t, key, String.t) :: :ok | no_return
+  def refute_key!(list, key, message \\ "Key is not allowed to be in list") do
+    if Keyword.has_key?(list, key) do
+      raise ArgumentError, message
+    else
+      :ok
+    end
+  end
+
+  @doc """
+  Raises an ArgumentError error if `list` does not have the provided `key`.
+
+  Takes an optional argument specifying the error message.
+
+  """
+  @spec assert_key!(t, key, String.t) :: :ok | no_return
+  def assert_key!(list, key, message \\ "Key is not allowed to be in list") do
+    unless Keyword.has_key?(list, key) do
+      raise ArgumentError, message
+    else
+      :ok
+    end
+  end
+end
---
+++ test/keyword/extra_test.exs
@@ -0,0 +1,6 @@
+defmodule Keyword.ExtraTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+  doctest Keyword.Extra
+end
