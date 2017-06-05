3153ce8a30f1ffc2f290b15b8f5ccf4e16f81504
William Carroll <wpcarro@gmail.com>
2017-06-05 17:08:19 -0400

    feat(extra:keyword): Adds default function to remove default value boilerplate

--- test/keyword/extra_test.exs
+++ test/keyword/extra_test.exs
@@ -3,4 +3,14 @@ defmodule Keyword.ExtraTest do
 
   use ExUnit.Case, async: true
   doctest Keyword.Extra
+
+  describe "default/3" do
+    test "Uses falsy values when they're supplied by the user" do
+      assert Keyword.Extra.default([optional: true, on_error: nil], :on_error, :error) == nil
+    end
+
+    test "Uses default values when the user does not supply a value" do
+      assert Keyword.Extra.default([optional: true], :on_error, :error) == :error
+    end
+  end
 end
