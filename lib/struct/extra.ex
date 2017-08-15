07c2f12e700f10808d91fa0d68f5cdfe1f46c2e5
William Carroll <wpcarro@gmail.com>
2017-08-15 19:10:02 -0400

    feat(extra:struct): Adds merge/2 for structs
    Map.merge was insufficient for merge since default values of struct B
    would overwrite values in struct A. This merging logic is more
    sophisticated since it accounts for a struct's default and non-default
    values.

--- lib/struct/extra.ex
+++ lib/struct/extra.ex
@@ -13,4 +13,42 @@ defmodule Struct.Extra do
   def drop(%{__struct__: s} = record, fields) do
     Map.merge(struct(s), Map.drop(record, fields))
   end
+
+
+  @doc """
+  Merges two of the same structs together.
+
+  Using `Map.merge/2` is not feasible because structs typically have default values of `nil`. This
+  function will not allow values of `nil` to overwrite non-nil values on merge.
+
+  """
+  @spec merge(struct, struct) :: struct
+  def merge(%{__struct__: same_struct} = a, %{__struct__: same_struct} = b)
+  when map_size(a) == map_size(b) do
+    defaults =
+      struct(same_struct)
+
+    result =
+      a
+      |> Map.from_struct
+      |> Enum.reduce(%{}, fn {k, a_val}, result ->
+        default =
+          Map.get(defaults, k)
+
+        case default do
+          # overwrite A if it has its default value
+          ^a_val ->
+            Map.put(result, k, Map.get(b, k))
+
+          _non_default ->
+            case Map.get(b, k) do
+              ^default -> Map.put(result, k, a_val)
+              b_val    -> Map.put(result, k, b_val)
+            end
+        end
+      end)
+
+    struct(same_struct, result)
+  end
+
 end
--- test/struct/extra_test.exs
+++ test/struct/extra_test.exs
@@ -22,7 +22,7 @@ defmodule Struct.ExtraTest do
       {:ok, ~M{input}}
     end
 
-    test "does not work for non-structs", ~M{input} do
+    test "does not work for non-structs" do
       assert_raise FunctionClauseError, fn ->
         Struct.Extra.drop(%{}, [])
       end
@@ -33,4 +33,60 @@ defmodule Struct.ExtraTest do
         %CreditCard{number: :default, cvv: :default}
     end
   end
+
+
+  describe "merge/2" do
+    test "overwrites default values in A with non-default values from B" do
+      a =
+        %CreditCard{number: :default}
+
+      b =
+        %CreditCard{number: "1234-5678-9101-1121"}
+
+      expected =
+        %CreditCard{number: "1234-5678-9101-1121"}
+
+      assert Struct.Extra.merge(a, b) == expected
+    end
+
+    test "does not overwrite non-default values in A with default values from B" do
+      a =
+        %CreditCard{number: "1234-5678-9101-1121"}
+
+      b =
+        %CreditCard{number: :default}
+
+      expected =
+        %CreditCard{number: "1234-5678-9101-1121"}
+
+      assert Struct.Extra.merge(a, b) == expected
+    end
+
+    test "overwrites non-default values in A with non-default values from B" do
+      a =
+        %CreditCard{cvv: "1234"}
+
+      b =
+        %CreditCard{cvv: "5678"}
+
+      expected =
+        %CreditCard{cvv: "5678"}
+
+      assert Struct.Extra.merge(a, b) == expected
+    end
+
+    test "overrides A's default values" do
+      a =
+        %CreditCard{cvv: :default}
+
+      b =
+        %CreditCard{cvv: "5678"}
+
+      expected =
+        %CreditCard{cvv: "5678"}
+
+      assert Struct.Extra.merge(a, b) == expected
+    end
+  end
+
 end
