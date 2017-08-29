28f506919081e8e76b7086fca749804e55a9cf2a
Griffin Smith <root@gws.fyi>
2017-08-29 16:34:02 -0400

    build(circleci): Add config for CircleCI
    Add config for running credo and dialyzer, plus config files for running
    them in CircleCI

--- mix.exs
+++ mix.exs
@@ -11,37 +11,27 @@ defmodule Extra.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     dialyzer: [
+       plt_add_deps: :project,
+       ignore_warnings: "./.dialyzer-ignore-warnings.txt"
+     ],
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:my_app, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:cortex, "~> 0.2.1", only: [:test, :dev], runtime: !ci_build?()},
       {:shorter_maps, "~> 2.1"},
+      {:flow, "~> 0.12", optional: true},
       {:propcheck, "~> 0.0", only: [:test]},
+      {:credo, "~> 0.8", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
     ]
   end
 
-  defp ci_build?, do: System.get_env("DRONE_COMMIT") != nil
+  defp ci_build?, do: System.get_env("DRONE_COMMIT") != nil || System.get_env("CI") != nil
 end
