122789337fbd13a468d31f01a73d60fdfde8cc55
Griffin Smith <root@gws.fyi>
2017-08-30 12:03:36 -0400

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Extra.Mixfile do
 
   def project do
     [app: :extra,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
