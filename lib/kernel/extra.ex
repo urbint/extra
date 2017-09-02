d7afb4b7dea20fd16fa3b8d3af593ef52e4a98fa
Griffin Smith <root@gws.fyi>
2017-09-02 13:55:03 -0400

    feat(kernel) add a defbang macro
    Add a `defbang/1` macro, for defining "banged" versions of functions to
    unwrap success values and return an error in the case of failure.

--- lib/kernel/extra.ex
+++ lib/kernel/extra.ex
@@ -22,4 +22,41 @@ defmodule Kernel.Extra do
   def boolean(x),
     do: if x, do: true, else: false
 
+  @doc """
+  Define a "banged" version of the given function/arities in the current module, which unwrap return
+  cases of the form `{:ok, result} | {:error, reason} | :error`, returning `result` in the success
+  case and throwing an `ArgumentError` in the error cases.
+
+  Functions are specified as keywords mapping function name to arity, just like the arguments to
+  `defoverridable` or `import`'s `:from` option
+
+  import
+
+    defmodule MyModule do
+      def only_even(x) when Integer.is_even(x), do: {:ok, x}
+      def only_even(_), do: {:error, "not even"}
+
+      defbang only_even: 1
+    end
+  """
+  @spec defbang([{atom, pos_integer}]) :: Macro.t
+  defmacro defbang(functions) do
+    Enum.map functions, fn {fname, arity} ->
+      banged =
+        :"#{fname}!"
+
+      args =
+        Macro.generate_arguments(arity, __MODULE__)
+
+      quote do
+        def unquote(banged)(unquote_splicing(args)) do
+          case unquote(fname)(unquote_splicing(args)) do
+            {:ok, result} -> result
+            {:error, msg} -> raise ArgumentError, msg
+            :error        -> raise ArgumentError
+          end
+        end
+      end
+    end
+  end
 end
--- test/kernel/extra_test.exs
+++ test/kernel/extra_test.exs
@@ -4,10 +4,9 @@ defmodule Kernel.ExtraTest do
 
   import Kernel.Extra
 
-  @falsy_vals [false, nil]
-
-
   describe "boolean" do
+    @falsy_vals [false, nil]
+
     property "returns true for truthy inputs" do
       forall x <- any() do
         implies x not in @falsy_vals do
@@ -20,6 +19,37 @@ defmodule Kernel.ExtraTest do
       forall x <- oneof(@falsy_vals) do
         boolean(x) == false
       end
+    end
+  end
+
+  describe "defbang" do
+    defmodule TestModule do
+      import Kernel.Extra
+      require Integer
+
+      def only_even(x) when Integer.is_even(x), do: {:ok, x}
+      def only_even(_), do: {:error, "not even"}
+
+      def only_odd(x) when Integer.is_odd(x), do: {:ok, x}
+      def only_odd(_), do: :error
+
+      defbang only_even: 1, only_odd: 1
+    end
+
+    test "returns the value when the wrapped function succeeds" do
+      assert TestModule.only_even(2) == {:ok, 2}
+      assert TestModule.only_even!(2) == 2
+
+      assert TestModule.only_odd(3) == {:ok, 3}
+      assert TestModule.only_odd!(3) == 3
+    end
+
+    test "throws an ArgumentError. when the wrapped function fails" do
+      assert {:error, msg} = TestModule.only_even(3)
+      assert_raise ArgumentError, msg, fn -> TestModule.only_even!(3) end
+
+      assert TestModule.only_odd(2) == :error
+      assert_raise ArgumentError, fn -> TestModule.only_even!(3) end
     end
   end
 end
