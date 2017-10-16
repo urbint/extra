0190be94ae18a9def53d969223bcef8850b70436
Griffin Smith <root@gws.fyi>
2017-10-16 15:01:43 -0400

    refactor(protocol): swap out module_implements? for `module_impl_for`
    To better mimic both the semantics and the naming of the standard
    library, swap out the `module_implements?` predicate for a
    `module_impl_for` function, which mirrors
    `SomeProtocolModule.impl_for/1` in both its naming and return value.

--- lib/protocol/extra.ex
+++ lib/protocol/extra.ex
@@ -5,23 +5,27 @@ defmodule Protocol.Extra do
   """
 
   @doc """
-  Returns true if the given *module* defines a struct which implements the given protocol.
+  Returns the protocol module implementing the given protocol for the given *module*, or `nil` if
+  the given module does not define a struct implementing the given protocol.
+
+  Note the difference betweeen this and `MyProto.impl_for/1`, which takes an *instance* of the
+  struct, whereas this takes the struct module itself.
 
   ## Examples
 
-      iex> Protocol.Extra.module_implements?(Enumerable, Stream)
-      true
+      iex> Protocol.Extra.module_impl_for(Enumerable, Stream)
+      Elixir.Enumerable.Stream
 
-      iex> Protocol.Extra.module_implements?(String.Chars, Stream)
-      false
+      iex> Protocol.Extra.module_impl_for(String.Chars, Stream)
+      nil
 
   """
-  @spec module_implements?(Protocol.t, module) :: boolean
-  def module_implements?(protocol, struct_module)
+  @spec module_impl_for(module, module) :: module | nil
+  def module_impl_for(protocol, struct_module)
   when is_atom(protocol) and is_atom(struct_module) do
     impl_mod =
       Module.concat([Elixir, protocol, struct_module])
 
-    :erlang.module_loaded(impl_mod)
+    if :erlang.module_loaded(impl_mod), do: impl_mod
   end
 end
