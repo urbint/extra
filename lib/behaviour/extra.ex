73a10fa5e2fa572ddf61ce86ae151af72a398059
Griffin Smith <root@gws.fyi>
2017-10-16 12:26:25 -0400

    feat(behaviour): Add Behaviour.Extra.implements?/2
    Add Behaviour.Extra.implements?, a predicate version of assert_impl?/2

--- lib/behaviour/extra.ex
+++ lib/behaviour/extra.ex
@@ -8,22 +8,14 @@ defmodule Behaviour.Extra do
 
 
   @doc """
-  Asserts that the passed behaviour's callbacks are all implemented
-  by the passed module.
+  Asserts that the passed behaviour's callbacks are all implemented by the passed module.
 
   Throws an `ArgumentError` if any are not implemented.
 
   """
   @spec assert_impl!(behaviour, module) :: :ok | no_return
   def assert_impl!(behaviour, module) do
-    callbacks =
-      callbacks(behaviour)
-
-    missing_functions =
-      callbacks
-      |> Enum.reject(fn {func, arity} -> function_exported?(module, func, arity) end)
-
-    case missing_functions do
+    case missing_functions(behaviour, module) do
       [] -> :ok
       missing when is_list(missing) ->
         formatted =
@@ -34,9 +26,29 @@ defmodule Behaviour.Extra do
     end
   end
 
+
+  @doc """
+  Returns true if the passed behaviour's callbacks are all implemented by the passed module.
+
+  """
+  @spec implements?(behaviour, module) :: boolean
+  def implements?(behaviour, module) do
+    behaviour
+    |> missing_functions(module)
+    |> Enum.empty?()
+  end
+
+
   @spec callbacks(behaviour) :: [{func :: atom, arity :: non_neg_integer}]
   defp callbacks(behaviour) do
     behaviour.behaviour_info(:callbacks)
+  end
+
+
+  @spec missing_functions(behaviour, module) :: [{func :: atom, arity :: non_neg_integer}]
+  defp missing_functions(behaviour, module) do
+    callbacks(behaviour)
+    |> Enum.reject(fn {func, arity} -> function_exported?(module, func, arity) end)
   end
 
 end
--- test/behaviour/extra_test.exs
+++ test/behaviour/extra_test.exs
@@ -1,7 +1,7 @@
 defmodule Behaviour.ExtraTest do
   use ExUnit.Case
 
-  describe "assert_impl!/2" do
+  describe "assert_impl!/2 and implements?/2" do
     defmodule Behave do
       @callback thing(any, any) :: boolean
     end
@@ -12,14 +12,22 @@ defmodule Behaviour.ExtraTest do
 
     defmodule ModB, do: nil
 
-    test "returns :ok if all functions are implemented" do
+    test "assert_impl!/2 returns :ok if all functions are implemented" do
       assert :ok == Behaviour.Extra.assert_impl!(Behave, ModA)
     end
 
-    test "raises when any function is not implemented" do
+    test "assert_impl!/2 raises when any function is not implemented" do
       assert_raise ArgumentError, fn ->
         Behaviour.Extra.assert_impl!(Behave, ModB)
       end
+    end
+
+    test "implements?/2 returns true if all functions are implemented" do
+      assert true == Behaviour.Extra.implements?(Behave, ModA)
+    end
+
+    test "implements?/2 returns false if any function is not implemented" do
+      assert false == Behaviour.Extra.implements?(Behave, ModB)
     end
   end
 end
